08c667ed701092cd2a649c9a94a3dfc4eefe2fd6
Shidoshi93 <dann.ribeiroo@gmail.com>
2021-07-24 00:55:36 +0100

    Adicionado status http relacionado com cada tipo de request.

--- src/main/java/br/com/api_mercadinho/controller/ProductController.java
+++ src/main/java/br/com/api_mercadinho/controller/ProductController.java
@@ -3,6 +3,7 @@ package br.com.api_mercadinho.controller;
 import br.com.api_mercadinho.model.Product;
 import br.com.api_mercadinho.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,16 +18,19 @@ public class ProductController {
     ProductRepository productRepository;
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
     public void addProduct(@RequestBody Product product){
         productRepository.save(product);
     }
 
     @RequestMapping(value = "/getProducts", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
     public List<Product> getProucts() {
         return (List<Product>) productRepository.findAll();
     }
 
     @RequestMapping(value = "/getProduct/{id}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
     public Product getProduct(@PathVariable Integer id){
         Optional<Product> product = productRepository.findById(id);
 
@@ -34,6 +38,7 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/updateProduct/{id}", method = RequestMethod.PATCH)
+    @ResponseStatus(HttpStatus.OK)
     public void updateProduct(@PathVariable Integer id, @RequestBody Product product) {
         productRepository.findById(id).map(record -> {
             record.setDesc(product.getDesc());
@@ -45,6 +50,7 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteProduct(@PathVariable Integer id) {
         productRepository.deleteById(id);
     }
